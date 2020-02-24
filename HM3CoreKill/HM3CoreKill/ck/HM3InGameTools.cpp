e55dd6b05f3ff01f26a6d4561979515e986005bf
DronCode <alexandrleutin@gmail.com>
2020-02-24 15:48:11 +0300

    Implemented play animation for player in actors viewer

--- HM3CoreKill/HM3CoreKill/ck/HM3InGameTools.cpp
+++ HM3CoreKill/HM3CoreKill/ck/HM3InGameTools.cpp
@@ -331,32 +331,6 @@ namespace ck
 				}
 				ImGui::Separator();
 			}
-
-			if (ImGui::Button("Test LockPick action"))
-			{
-				const auto& doorsRegistry = HM3DoorsRegistry::getRegistry();
-				const auto& animationsRegistry = HM3AnimationRegistry::getRegistry();
-
-				auto pAction = reinterpret_cast<ioi::hm3::ZHitmanActionPickLock*>(HM3ActionFactory::createActionByType(hitman3, ioi::hm3::ZLnkActionType::HitmanActionPickLock));
-				HM3_ASSERT(pAction != nullptr, "Action Factory must return not null pointer!");
-
-				auto pDoor = doorsRegistry.getDoorByName("D03A_Door_Auditorium_01");
-				HM3_ASSERT(pDoor != nullptr, "DoorRegistry| Unable to find door!");
-
-				pAction->m_door				 = pDoor->m_doorSTD;
-				pAction->m_animPickLockStart = animationsRegistry.getAnimationByName("anim:Hero/Hero#Pick_Lock_Start");
-				pAction->m_animPickLockStop  = animationsRegistry.getAnimationByName("anim:Hero/Hero#Pick_Lock_Stop");
-				pAction->m_animPickLockLoop  = animationsRegistry.getAnimationByName("anim:Hero/Hero#Pick_Lock_Loop");
-
-				HM3_ASSERT(pAction->m_door              != nullptr, "Door:STDOBJ    must be loaded!");
-				HM3_ASSERT(pAction->m_animPickLockStart != nullptr, "PickLock:Start animation must be loaded!");
-				HM3_ASSERT(pAction->m_animPickLockStop  != nullptr, "PickLock:Stop  animation must be loaded!");
-				HM3_ASSERT(pAction->m_animPickLockLoop  != nullptr, "PickLock:Loop  animation must be loaded!");
-
-				HM3_DEBUG("Trying to perform action %s\n", pAction->GetActionName());
-				
-				pAction->Function2();
-			}
 		}
 	}
 
@@ -403,7 +377,8 @@ namespace ck
 					};
 					
 					ImGui::Text("Location pointer at 0x%.8X", currentActor->ActorInformation->location);
-					ImGui::Text("Position:"); ImGui::SameLine(0.f, 4.f); ImGui::InputFloat3("", actorPosition); 
+					ImGui::Text("Position:"); ImGui::SameLine(0.f, 4.f); ImGui::InputFloat3("", actorPosition);
+					ImGui::Text("Inventory: 0x%.8X", currentActor->ActorInformation->equipment);
 
 					{
 						ImGui::Text("Set animation: "); ImGui::SameLine(0.f, 5.f);
@@ -440,6 +415,13 @@ namespace ck
 						{
 							currentActor->dropAnimation(6, 0);
 							currentActor->setAnimation(currentAnim);
+						}
+
+						ImGui::SameLine(0.f, 5.f);
+						if (currentAnim && ImGui::Button("Apply for player"))
+						{
+							gameData->m_Hitman3->dropCurrentAnimation();
+							gameData->m_Hitman3->setAnimation(currentAnim);
 						}
 					}
 
